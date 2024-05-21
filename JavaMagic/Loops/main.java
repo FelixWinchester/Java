c381e68cf0e758a8708ac354233ed0ed3746ba7a
Winchester <113387249+FelixWinchester@users.noreply.github.com>
2024-05-21 23:26:23 +0400

    Create main.java

---
+++ JavaMagic/Loops/main.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.lang.*;
+
+public class Main{
+    public static void main(String[] args){
+        Spell cl = new Spell();
+        cl.Values();
+        cl.Game();
+        cl.Chetnoe();
+    }
+}
