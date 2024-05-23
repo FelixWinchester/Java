29746fc1e924afefb3c665b9fa4d16c7376dfca9
Winchester <113387249+FelixWinchester@users.noreply.github.com>
2024-05-23 12:58:37 +0400

    Create Main.java

---
+++ JavaMagic/Arrays/Main.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.lang.*;
+
+public class Main{
+    public static void main(String[] args){
+//        String[] recipe = {"Засушенная чешуя василиска",
+//                "Частичка души лесной феи",
+//                "Капля сока вампирской розы",
+//                "Яд шершавого ядозуба",
+//                "Осколок кометы",
+//                "Слеза феникса"};
+//        String value = Arrays.toString(recipe);
+//System.out.println(value);
+// Порядок добавления: Капля сока вампирской розы,
+// Слеза феникса, Засушенная чешуя василиска, Частичка души лесной феи,
+// Яд шершавого ядозуба, Осколок кометы
+//        Scanner in = new Scanner(System.in);
+//        String[] potion = new String[6];
+//
+//        for (int i = 0; i < 6; i++){
+//            String m = in.nextLine();
+//            potion[i] = m;
+//        }
+//        boolean output = Arrays.equals(recipe, potion);
+//        System.out.println(output);
+
+      Spell mg = new Spell();
+//        mg.spell();
+        //mg.Potion();
+        mg.arrayaz();
+    }
+}
